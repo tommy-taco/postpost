4f620c2b45b9ee4aa0bf3792d47b7b288560f61b
AJ <ajhoward14@gmail.com>
2013-12-29 18:58:21 -0800

    start me up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SepAlum::Application.config.secret_key_base = 'bbadc113c64ff23a2187326ba79ded47250eb8bb03992170ec031ba183fe6a4f1149c4102796c89a8ab2f34fb7baff2a7f05adcdd9ca18243a4d42b78c0743ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SepAlum::Application.config.session_store :cookie_store, key: '_sep_alum_session'
