f2b85c4c7dc91f88d642896f12881dc6c7f9d72a
AJ <ajhoward14@gmail.com>
2014-01-06 16:54:04 -0800

    finished signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,24 @@ class UsersController < ApplicationController
   end
 
   def new
-  	@user = User.new
-  	@organizations = Organization.all
+   	@user = User.new
+   	@organizations = Organization.all
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the PostPost!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+ 		params.require(:user).permit(:name,:email, :password,
+  									:password_confirmation,:organization_id)
+    end
 end
