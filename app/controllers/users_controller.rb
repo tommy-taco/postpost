58b1fd837bb3811e8af99818557c2a99f4b77d36
AJ <ajhoward14@gmail.com>
2014-01-10 18:03:12 -0800

    if i fucked up it was here

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+
 
   def show
     @user = User.find(params[:id])
@@ -20,10 +24,33 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
     def user_params
  		params.require(:user).permit(:name,:email, :password,
   									:password_confirmation,:organization_id)
+    end
+    
+# Before filters
+
+	def signed_in_user
+	  redirect_to signin_url, notice: "Please sign in." unless signed_in?
+	end
+	
+	def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
