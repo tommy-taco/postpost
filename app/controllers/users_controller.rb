dde23e8598de8683b52adee3cd7602add43f8f9f
AJ <ajhoward14@gmail.com>
2014-01-13 19:42:12 -0800

    finished 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the PostPost!"
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
