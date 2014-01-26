7b40303481d53c58ddf6506a36d94e8f11a13145
AJ <ajhoward14@gmail.com>
2014-01-25 19:12:22 -0800

    finished org REST

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,8 @@
 class OrganizationsController < ApplicationController
+	before_action :signed_in_user
+	before_action :admin_user,     only: :destroy	
+
+
   def new
   	@organization = Organization.new
   end
@@ -17,9 +21,34 @@ class OrganizationsController < ApplicationController
   	@organization = Organization.find(params[:id])
   end
   
+  def edit
+    @organization = Organization.find(params[:id])
+  end
+  
+  def update
+    @organization = Organization.find(params[:id])
+    if @organization.update_attributes(org_params)
+      flash[:success] = "Network updated"
+      redirect_to @organization
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+  	@organization = Organization.find(params[:id])
+    Organization.find(params[:id]).destroy
+    flash[:success] = "Organization deleted."
+    redirect_to root_path
+  end
+  
   private
   	
   	def org_params
  		params.require(:organization).permit(:name)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
